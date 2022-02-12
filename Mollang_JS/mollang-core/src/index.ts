0bd8536095440a35b0072cc82a3209be6118b3f8
kndlr <47521176+kndlr@users.noreply.github.com>
2022-02-12 23:24:50 +0900

    Fix: 출력, 에러 함수 설정

--- Mollang_JS/mollang-core/src/index.ts
+++ Mollang_JS/mollang-core/src/index.ts
@@ -6,14 +6,22 @@ import exitMessage from "./exit";
  * Mollang 실행
  * @param code 실행할 Mollang 코드
  * @param maxRecursion GOTO 반복 최대 가능 횟수 (100000)
+ * @param outputFn 시스템 출력 함수 (string 전달)
+ * @param errorFn 시스템 오류 함수 ([number, string] 전달)
  * @param inputFn 사용자 입력 함수 (string 반환)
  * @returns [출력, 오류 코드, 오류, 파싱 결과]
  */
 export default function main(
     code: string,
-    maxRecursion: number = 100000,
-    inputFn: () => string
+    options: {
+        maxRecursion?: number;
+        outputFn?: (msg: string) => void;
+        errorFn?: (code: number, msg: string) => void;
+        inputFn: () => string;
+    }
 ) {
+    const { maxRecursion = 100000, outputFn, errorFn, inputFn } = options;
+
     code = code
         .split("\n")
         .map((line: string) => line.trim())
@@ -25,14 +33,19 @@ export default function main(
 
     const parsed = parse(code, (...args) => {
         const [code, msg] = exitMessage(...args);
+        errorFn && errorFn(code, msg);
         errorCodes.push(code);
         errors.push(msg);
     });
     run(
         maxRecursion,
-        (msg) => outputs.push(msg.toString()),
+        (msg) => {
+            outputFn && outputFn(msg);
+            outputs.push(msg.toString());
+        },
         (...args) => {
             let [code, msg] = exitMessage(...args);
+            errorFn && errorFn(code, msg);
             errorCodes.push(code);
             errors.push(msg);
         },
