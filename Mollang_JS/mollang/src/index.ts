ea8d38e6ba06aa02ced133b09150046fedc675a8
kndlr <47521176+kndlr@users.noreply.github.com>
2022-02-14 00:12:18 +0900

    Add: CORE 1.1 대응

--- Mollang_JS/mollang/src/index.ts
+++ Mollang_JS/mollang/src/index.ts
@@ -8,12 +8,7 @@ program
     .name("mollang")
     .argument("<filename>")
     .option("-p, --parse <outfile>", "Write parsed data to file", false)
-    .option(
-        "-r, --recursion <times>",
-        "Max number of recursions",
-        Number,
-        100000
-    )
+    .option("-r, --recursion <times>", "Max number of recursions", Number, 100000)
     .action((fn: string, options) => {
         if (fn.endsWith(".molu")) {
             if (fs.existsSync(fn)) {
@@ -21,21 +16,17 @@ program
                     if (err) {
                         throw err;
                     } else {
-                        const [_outputs, _errorCodes, _errors, parsed] = main(
-                            data,
-                            {
-                                maxRecursion: options.recursion,
-                                outputFn: (msg) => process.stdout.write(msg),
-                                errorFn: (_code, msg) =>
-                                    process.stderr.write(msg),
-                                inputFn: readline.question,
-                            }
-                        );
+                        const [_outputs, _errorCodes, _errorPositions, parsed] = main(data, {
+                            maxRecursion: options.recursion,
+                            outputFn: (msg) => process.stdout.write(msg),
+                            errorFn: (code, position) =>
+                                process.stderr.write(
+                                    `* ${position}번째 줄에서 오류 코드 ${code} (이)가 발생했습니다.\n`
+                                ),
+                            inputFn: readline.question,
+                        });
                         if (options.parse) {
-                            fs.writeFileSync(
-                                options.parse,
-                                JSON.stringify(parsed)
-                            );
+                            fs.writeFileSync(options.parse, parsed);
                         }
                     }
                 });
